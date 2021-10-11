02fe2751e568354d09721ed2e4742bd90d816160
zettlrobert <zettl.robert@gmail.com>
2021-10-11 02:01:36 +0200

    App.tsx with first component integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Gallery from "./components/Gallery";
+import styles from "./App.module.scss";
+import { GalleryVariant } from "./components/Gallery/Gallery.types";
+import TopBar from "./components/TopBar/TopBar";
+import ToggleSwitch from "./components/ToggleSwitch";
+import { useState } from "react";
+import useIsMobile from "./hooks/useIsMobile";
 
 function App() {
+  const isMobile = useIsMobile();
+
+  // Gallery Variant in App State
+  const [galleryVariant, setGalleryVariant] = useState<GalleryVariant>(
+    GalleryVariant.FULL
+  );
+
+  // Initialize empty array for mock Items
+  let mockElements = [];
+
+  // Count of Mock Itemsx
+  const numberOfElements = 10;
+
+  // Generate and fill Mock Item Array
+  for (let i = 0; i < numberOfElements; i++) {
+    mockElements.push(<div className={styles.mockItem}>Item: {i}</div>);
+  }
+
+  // Toggle between Gallery Variants
+  const toggleGalleryVariant = () => {
+    let updatedGalleryVaraint =
+      galleryVariant === GalleryVariant.FULL
+        ? GalleryVariant.INSET
+        : GalleryVariant.FULL;
+
+    setGalleryVariant(updatedGalleryVaraint);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar>
+        {!isMobile && <h1>React Gallery ZR</h1>}
+        <div className={styles.GalleryToggleContainer}>
+          <p>
+            Gallery Variant: <span>{galleryVariant}</span>
+          </p>
+          <ToggleSwitch onClick={toggleGalleryVariant} />
+        </div>
+      </TopBar>
+
+      <main>
+        <Gallery variant={galleryVariant}>
+          {mockElements.map((element, index) => (
+            <div key={`element-index${index}`}>{element}</div>
+          ))}
+        </Gallery>
+      </main>
     </div>
   );
 }
